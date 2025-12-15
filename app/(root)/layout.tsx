433c050d61eb3d68daa4607d4fe83887ab61bc54
sameerjohn1 <sameerprogrammer5@gmail.com>
2025-12-15 17:35:57 +0500

    implement header section, responsive on all devices

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,4 @@
-import Header from "@/components/landing/Header";
+import { Header } from "@/components/landing/Header";
 import Footer from "@/components/layout/footer";
 import { Metadata } from "next";
 

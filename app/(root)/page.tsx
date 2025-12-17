2cf807f9991d1ed5ff4fbe4b7db1b51c4ef5f60e
sameerjohn1 <sameerprogrammer5@gmail.com>
2025-12-17 12:46:49 +0500

    codesnippet components inputs added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,20 @@
+import AIInput_01 from "@/components/codesnippetui/ai-input/ai-input-01";
+import AIInput_02 from "@/components/codesnippetui/ai-input/ai-input-02";
+import AIInput_03 from "@/components/codesnippetui/ai-input/ai-input-03";
+import AIInput_04 from "@/components/codesnippetui/ai-input/ai-input-04";
+import AIInput_05 from "@/components/codesnippetui/ai-input/ai-input-05";
+import AIInput_06 from "@/components/codesnippetui/ai-input/ai-input-06";
+import AIInput_07 from "@/components/codesnippetui/ai-input/ai-input-07";
+import AIInput_08 from "@/components/codesnippetui/ai-input/ai-input-08";
+import AIInput_09 from "@/components/codesnippetui/ai-input/ai-input-09";
+import AIInput_10 from "@/components/codesnippetui/ai-input/ai-input-10";
+import AIInput_11 from "@/components/codesnippetui/ai-input/ai-input-11";
+import AIInput_12 from "@/components/codesnippetui/ai-input/ai-input-12";
+import AIInput_13 from "@/components/codesnippetui/ai-input/ai-input-13";
+import AIInput_14 from "@/components/codesnippetui/ai-input/ai-input-14";
+import AIInput_15 from "@/components/codesnippetui/ai-input/ai-input-15";
+import AIInput_16 from "@/components/codesnippetui/ai-input/ai-input-16";
+import AIInput_17 from "@/components/codesnippetui/ai-input/ai-input-17";
 import HeroSection from "@/components/landing/hero";
 
 export default function Home() {
@@ -5,6 +22,26 @@ export default function Home() {
     <main className="bg-white dark:bg-black/5 overflow-x-hidden">
       <div className="grid grid-rows-[auto_1fr_auto] min-h-screen px-6 lg:px-4 gap-4 sm:gap-12">
         <HeroSection />
+      </div>
+
+      <div className="flex flex-col justify-center gap-10 max-w-7xl items-center mx-auto">
+        {/* <AIInput_01 />
+        <AIInput_02 />
+        <AIInput_03 />
+        <AIInput_04 />
+        <AIInput_05 />
+        <AIInput_06 />
+        <AIInput_07 />
+        <AIInput_08 />
+        <AIInput_09 />
+        <AIInput_10 />
+        <AIInput_11 />
+        <AIInput_12 />
+        <AIInput_13 />
+        <AIInput_14 />
+        <AIInput_15 />
+        <AIInput_16 />
+        <AIInput_17 /> */}
       </div>
     </main>
   );
